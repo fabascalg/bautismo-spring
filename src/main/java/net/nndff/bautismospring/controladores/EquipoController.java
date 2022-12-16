96c9533c78912389ab97f521c5ae9529d3ee6c95
fabascalg <fagmsm@gmail.com>
2022-12-17 00:45:01 +0100

    tratando de subir por enesima vez...

--- src/main/java/net/nndff/bautismospring/controladores/EquipoController.java
+++ src/main/java/net/nndff/bautismospring/controladores/EquipoController.java
@@ -1,14 +1,15 @@
 package net.nndff.bautismospring.controladores;
 
 import net.nndff.bautismospring.modelo.Equipo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @RestController
+@RequestMapping("/equipos")
 public class EquipoController {
     ArrayList<Equipo> equipos = new ArrayList<>(
             // List.of ¡¡¡ buscar documentación... es la primera vez que la uso, o es que no me acuerdo?
@@ -21,19 +22,57 @@ public class EquipoController {
             )
     );
 
-    @GetMapping("/equipos")
+    @GetMapping
     public ArrayList<Equipo> listarTodos() {
         return equipos;
     }
 
-    @GetMapping("/equipos/{nombre}")
-    public Equipo recuperarEquipoPorNombre(@PathVariable String nombre){
-        for (Equipo equipo: equipos){
-            if (equipo.getNombrePais().equalsIgnoreCase(nombre)){
+    @GetMapping("/{nombre}")
+    public Equipo recuperarEquipoPorNombre(@PathVariable String nombre) {
+        for (Equipo equipo : equipos) {
+            if (equipo.getNombrePais().equalsIgnoreCase(nombre)) {
                 return equipo;
             }
         }
         return null;
+    }
+
+    @PostMapping
+    public void crearEquipo(@RequestBody Equipo equipo){
+        equipos.add(equipo);
+    }
+
+    @DeleteMapping("/{nombre}")
+    public void eliminarEquipo(@PathVariable String nombre) {
+        /*
+        for (Equipo equipo: equipos){
+            if (equipo.getNombrePais().equalsIgnoreCase(nombre)){
+                equipos.remove(equipo);
+            }
+        }
+         */
+        Optional<Equipo> optEquipo = equipos.stream().filter(equipo -> equipo.getNombrePais().equalsIgnoreCase(nombre)).findFirst();
+        if (optEquipo.isPresent()){
+            equipos.remove(optEquipo.get());
+        }
+    }
+
+    @PutMapping
+    public void modificarEquipo(@RequestBody Equipo equipoModif){
+        for (Equipo equipo: equipos){
+            if (equipo.getNombrePais().equalsIgnoreCase(equipoModif.getNombrePais())){
+                equipo.setConf(equipoModif.getConf());
+                equipo.setRankingFifa(equipoModif.getRankingFifa());
+            }
+        }
+    }
+
+    @GetMapping("/jugar/{equipoA}/{equipoB}")
+    public String jugar(@PathVariable String equipoA,@PathVariable String equipoB){
+        Random random = new Random();
+        int golesA = random.nextInt(10);
+        int golesB = random.nextInt(4);
+        return "Equipo " + equipoA + "(" + golesA + ") - EquipoB " +  equipoB + "(" + golesB + ")";
     }
 
 }
